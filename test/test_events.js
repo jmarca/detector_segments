c5bc54f9f1400b568d3a1e3b999927ec82a7d60d
James E. Marca <james@activimetrics.com>
2015-07-22 14:51:47 -0700

    more test: code matches hand calculation of output

--- test/test_events.js
+++ test/test_events.js
@@ -15,35 +15,60 @@ describe('generate event times',function(){
         var group = [
             {
                 'id':'A',
-                'mints':'2012-09-06 14:00:00',
-                'maxts':'2012-12-28 09:00:00'
+                'mints':'2012-09-06 14:00:00', //
+                'maxts':'2012-12-28 09:00:00'  //
             },
             {
                 'id':'B',
-                'mints':'2012-09-16 03:00:00',
-                'maxts':'2012-12-28 09:00:00'
+                'mints':'2012-09-16 03:00:00', //
+                'maxts':'2012-12-28 09:00:00'  //
             },
             {
                 'id':'C',
-                'mints':'2012-09-06 16:00:00',
-                'maxts':'2012-09-12 00:00:00'
+                'mints':'2012-09-06 16:00:00', //
+                'maxts':'2012-09-12 00:00:00'  //
             },
             {
                 'id':'D',
-                'mints':'2012-09-12 14:00:00',
-                'maxts':'2012-12-28 09:00:00'
+                'mints':'2012-09-12 14:00:00', //
+                'maxts':'2012-12-28 09:00:00'  //
             },
             {
                 'id':'E',
-                'mints':'2012-10-29 02:00:00',
+                'mints':'2012-10-29 02:00:00', //
                 'maxts':'2012-12-28 18:00:00'
             }
         ]
         var result = generate_event_times(group)
-        console.log(result)
+        // console.log(result)
         should.exist(result)
         result.should.be.instanceOf(Array)
         result.should.have.lengthOf(7)
+        result.forEach(function(row){
+            row.should.have.keys('ts','detectors')
+            return null
+        })
+        result[0].ts.should.eql('2012-09-06 14:00:00')
+        result[0].detectors.should.eql(['A'])
+
+        result[1].ts.should.eql('2012-09-06 16:00:00')
+        result[1].detectors.should.eql(['A','C'])
+
+        result[2].ts.should.eql('2012-09-12 01:00:00')
+        result[2].detectors.should.eql(['A'])
+
+        result[3].ts.should.eql('2012-09-12 14:00:00')
+        result[3].detectors.should.eql(['A','D'])
+
+        result[4].ts.should.eql('2012-09-16 03:00:00')
+        result[4].detectors.should.eql(['A','B','D'])
+
+        result[5].ts.should.eql('2012-10-29 02:00:00')
+        result[5].detectors.should.eql(['A','B','D','E'])
+
+        result[6].ts.should.eql('2012-12-28 10:00:00')
+        result[6].detectors.should.eql(['E'])
+
 
     })
 })
