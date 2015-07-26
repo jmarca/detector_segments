233b23318681c40f60443751a7859c608df9705b
James E. Marca <james@activimetrics.com>
2015-07-26 08:30:03 -0700

    Note the edits that I need to do

--- lib/reduced_sql.sql
+++ lib/reduced_sql.sql
@@ -1,8 +1,20 @@
-select components,min(ts) as ts, max(endts) as endts, condition, direction from tempseg2.detector_segment_conditions where ts > '2012-01-01' and endts <= '2013-01-01' group by components,condition,direction order by components,ts limit 10;
+--
+--  this sql will reduce the multiple entries in detector_segment_conditions
+--  into single entries in reduced_detector_segment_conditions
+--
+--  needs to be tweaked though.  At the moment it presumes that all
+--  rows are continuous...that is, if component A has an entry at time
+--  t1 and max entry at tn, the presumption is that component A also
+--  has an entry for every time step from 1 to n.  In fact there is no
+--  guarantee that this is the case.  In the worst case, it could be
+--  that a component is only active in the first hour and the last
+--  hour of a year, but the below query will screw up and make it
+--  active for every hour.
+--
+
 
 insert into tempseg.reduced_detector_segment_conditions
 (components,ts, endts, condition, direction)
-
 with reduction as (
     SELECT components,min(ts) as ts, max(endts) as endts, condition, direction
     FROM tempseg2.detector_segment_conditions
